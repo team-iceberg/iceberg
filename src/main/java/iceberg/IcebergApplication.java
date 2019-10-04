50e07f70d20453abaa67f61bd7a4df3206d41f9f
Levoye Camille <camille.levoye@ext.leroymerlin.fr>
2019-10-04 11:51:10 +0200

    feat(init): init project

---
+++ src/main/java/iceberg/IcebergApplication.java
@@ -0,0 +1,13 @@
+package iceberg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcebergApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcebergApplication.class, args);
+	}
+
+}
