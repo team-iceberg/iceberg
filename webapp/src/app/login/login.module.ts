1b4a4071a871f66f96e4800db778398051b31442
Camille LEVOYE <camille.levoye@ext.leroymerlin.fr>
2020-01-17 14:14:39 +0100

    feat(login): finalize style for login page

--- webapp/src/app/login/login.module.ts
+++ webapp/src/app/login/login.module.ts
@@ -1,4 +1,10 @@
+import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {TranslateModule} from '@ngx-translate/core';
+import {MaterialModule} from '../shared/material/material.module';
+import {SharedModule} from '../shared/shared.module';
 
 import {LoginComponent} from './component/login.component';
 import {LoginRoutingModule} from './login-routing.module';
@@ -9,7 +15,14 @@ import {LoginRoutingModule} from './login-routing.module';
         LoginComponent
     ],
     imports: [
-        LoginRoutingModule
+        CommonModule,
+        MaterialModule,
+        LoginRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        SharedModule,
+        FlexLayoutModule,
+        TranslateModule
     ],
     providers: [],
     bootstrap: [LoginComponent]
