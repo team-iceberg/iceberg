6cd31ce85ab5d1aa0ed8e4859589889bbf1ed01e
Camille LEVOYE <camille.levoye@ext.leroymerlin.fr>
2020-02-19 06:57:05 +0100

    fix(login): correction login

--- webapp/src/app/home/home.module.ts
+++ webapp/src/app/home/home.module.ts
@@ -4,10 +4,10 @@ import {TranslateModule} from '@ngx-translate/core';
 import {AssociationService} from '../services/association/association.service';
 import {ColorService} from '../services/color/color.service';
 import {SizeService} from '../services/size/size.service';
-import {MaterialModule} from '../tools/material/material.module';
 import {HomeComponent} from './component/home.component';
 import {HeaderComponent} from './header/header.component';
 import {HomeRoutingModule} from './home-routing.module';
+import {MaterialModule} from "../shared/material/material.module";
 
 @NgModule({
     declarations: [
--- webapp/src/app/login/component/login.component.ts
+++ webapp/src/app/login/component/login.component.ts
@@ -2,7 +2,6 @@ import {Component, NgZone} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService, GoogleLoginProvider} from 'angularx-social-login';
 import {AuthenticationService} from '../../services/authentication.service';
-import {throwError} from "rxjs";
 import {TranslateService} from "@ngx-translate/core";
 import {ToastService} from "../../shared/toast/toast.service";
 
@@ -20,41 +19,31 @@ export class LoginComponent {
     private toaster: ToastService, private zone: NgZone) {
   }
 
-    socialSignIn(socialProvider: string) {
-        let socialPlatformProvider;
-        if (socialProvider === 'google') {
-            socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
-        }
-        this.OAuth.signIn(socialPlatformProvider).then(socialusers => {
-            this.authenticationService.user = socialusers;
-            this.authenticationService.authenticateUser(socialusers.idToken).subscribe(value => {
-              this.authenticationService.user = value;
-              console.log(this.router.url);
-              console.log(this.router.url);
-            });
-          this.router.navigate(['home']);
-        });
+  socialSignIn(socialProvider: string) {
+    let socialPlatformProvider;
+    if (socialProvider === 'google') {
+      socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
     }
+    this.OAuth.signIn(socialPlatformProvider).then(socialusers => {
+      this.authenticationService.user = socialusers;
+    }).catch(reason => {
+      console.log("erreur", reason);
+      let errMsg = this.translate.instant("login.error.500");
+      this.toaster.alert(errMsg, 3000);
+    }).finally(() => {
+      this.authenticationService.authenticateUser(this.authenticationService.user.idToken).subscribe(user => {
+        this.router.navigate(['home']);
+      }, error => {
+        console.log("erreur", error);
+        let errMsg: string = this.translate.instant("login.error.500");
 
+        if (error.status == 403) {
+          errMsg = this.translate.instant("login.error.403");
+        }
+        
+        this.toaster.alert(errMsg, 3000);
+      });
+    });
 
-  // socialSignIn(socialProvider: string) {
-  //   let socialPlatformProvider;
-  //   if (socialProvider === 'google') {
-  //     socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
-  //   }
-  //   this.OAuth.signIn(socialPlatformProvider).then(socialusers => {
-  //     this.authenticationService.authenticateUser(socialusers.idToken).subscribe(user => {
-  //       this.authenticationService.user = user;
-  //       this.router.navigate(['home']);
-  //     }, error => {
-  //       console.log("erreur", error);
-  //       let errMsg: string;
-  //       if (error.status != 401 && error.status != 403) {
-  //         errMsg = this.translate.instant("login.error.500");
-  //         this.toaster.alert(errMsg, 3000);
-  //       }
-  //       this.toaster.alert(errMsg);
-  //     });
-  //   });
-  // }
+  }
 }
