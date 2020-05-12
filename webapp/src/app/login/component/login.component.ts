9f90308786fcb450bf053e4245d4858b57da74c5
Camille LEVOYE <camille.levoye@ext.leroymerlin.fr>
2020-05-12 20:04:16 +0200

    feat(login): change style and change forme

--- webapp/src/app/login/component/login.component.ts
+++ webapp/src/app/login/component/login.component.ts
@@ -1,48 +1,52 @@
 import {Component, NgZone} from '@angular/core';
 import {Router} from '@angular/router';
+import {TranslateService} from '@ngx-translate/core';
 import {AuthService, GoogleLoginProvider} from 'angularx-social-login';
 import {AuthenticationService} from '../../services/authentication.service';
-import {TranslateService} from "@ngx-translate/core";
-import {ToastService} from "../../shared/toast/toast.service";
+import {ToastService} from '../../shared/toast/toast.service';
 
 @Component({
-  selector: 'alogin',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.less']
+    selector: 'alogin',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.less']
 })
 export class LoginComponent {
 
-  constructor(
-    public OAuth: AuthService, private router: Router,
-    private authenticationService: AuthenticationService,
-    private translate: TranslateService,
-    private toaster: ToastService, private zone: NgZone) {
-  }
-
-  socialSignIn(socialProvider: string) {
-    let socialPlatformProvider;
-    if (socialProvider === 'google') {
-      socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+    constructor(
+        public OAuth: AuthService, private router: Router,
+        private authenticationService: AuthenticationService,
+        private translate: TranslateService,
+        private toaster: ToastService, private zone: NgZone) {
     }
-    this.OAuth.signIn(socialPlatformProvider).then(socialusers => {
-      this.authenticationService.user = socialusers;
-    }).catch(reason => {
-      console.log("erreur", reason);
-      let errMsg = this.translate.instant("login.error.500");
-      this.toaster.alert(errMsg, 3000);
-    }).finally(() => {
-      this.authenticationService.authenticateUser(this.authenticationService.user.idToken).subscribe(user => {
-        this.router.navigate(['home']);
-      }, error => {
-        console.log("erreur", error);
-        let errMsg: string = this.translate.instant("login.error.500");
 
-        if (error.status == 403) {
-          errMsg = this.translate.instant("login.error.403");
+    socialSignIn(socialProvider: string) {
+        let socialPlatformProvider;
+        if (socialProvider === 'google') {
+            socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
         }
-        
-        this.toaster.alert(errMsg, 3000);
-      });
-    });
-  }
+        this.OAuth.signIn(socialPlatformProvider).then(socialusers => {
+            this.authenticationService.user = socialusers;
+        }).catch(reason => {
+            console.log('erreur', reason);
+            let errMsg = this.translate.instant('login.error.500');
+            this.toaster.alert(errMsg, 3000);
+        }).finally(() => {
+            this.authenticationService.authenticateUser(this.authenticationService.user.idToken).subscribe(user => {
+                this.router.navigate(['home']);
+            }, error => {
+                console.log('erreur', error);
+                let errMsg: string = this.translate.instant('login.error.500');
+
+                if (error.status == 403) {
+                    errMsg = this.translate.instant('login.error.403');
+                }
+
+                this.toaster.alert(errMsg, 3000);
+            });
+        });
+    }
+
+    contactUs() {
+        this.toaster.info("Vous nous avez contacté", 3000);
+    }
 }
--- webapp/src/app/shared/material/material.module.ts
+++ webapp/src/app/shared/material/material.module.ts
@@ -1,42 +1,47 @@
 import {NgModule} from '@angular/core';
+import {FlexModule, FlexLayoutModule} from '@angular/flex-layout';
 import {
-  MatButtonModule,
-  MatDividerModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatProgressSpinnerModule,
-  MatSnackBarModule,
-  MatTableModule,
-  MatToolbarModule
+    MatButtonModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatToolbarModule
 } from '@angular/material';
 
 @NgModule({
-  imports: [
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatTableModule,
-    MatIconModule,
-    MatListModule,
-    MatDividerModule,
-    MatProgressSpinnerModule,
-    MatToolbarModule,
-    MatSnackBarModule
-  ],
-  exports: [
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatTableModule,
-    MatIconModule,
-    MatListModule,
-    MatDividerModule,
-    MatProgressSpinnerModule,
-    MatToolbarModule,
-    MatSnackBarModule
-  ]
+    imports: [
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatTableModule,
+        MatIconModule,
+        MatListModule,
+        MatDividerModule,
+        MatProgressSpinnerModule,
+        MatToolbarModule,
+        MatSnackBarModule,
+        FlexModule,
+        FlexLayoutModule
+    ],
+    exports: [
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatTableModule,
+        MatIconModule,
+        MatListModule,
+        MatDividerModule,
+        MatProgressSpinnerModule,
+        MatToolbarModule,
+        MatSnackBarModule,
+        FlexModule,
+        FlexLayoutModule
+    ]
 })
 export class MaterialModule {
 }
